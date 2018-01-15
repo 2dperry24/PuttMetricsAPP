72a46576dea27e40a5d020b88199db4cb42d228e
Dustin Perry <2dperry24@gmail.com>
2018-01-14 21:23:47 -0500

    operational tictactoe before trying to get it to show color on the other devices before sending the x or o.

--- PuttMetricsAPP/Controller/TicTacToe.swift
+++ PuttMetricsAPP/Controller/TicTacToe.swift
@@ -7,8 +7,16 @@
 //
 
 import UIKit
+import MultipeerConnectivity
 
-class TicTacToe: UIViewController {
+class TicTacToe: UIViewController, MCSessionDelegate, MCBrowserViewControllerDelegate {
+    
+    
+    var peerID:MCPeerID!
+    var mcSession:MCSession!
+    var mcAdvertiserAssistant:MCAdvertiserAssistant!
+    
+    
 
     @IBOutlet weak var gridImage: UIImageView!
     @IBOutlet weak var gridHeightConstraint: NSLayoutConstraint!
@@ -19,7 +27,7 @@ class TicTacToe: UIViewController {
     @IBOutlet weak var player2View: DesignableView!
     @IBOutlet weak var player1Container: DesignableView!
     @IBOutlet weak var player2Container: DesignableView!
-    
+
    
     
     private var lowerBound = 10
@@ -28,11 +36,12 @@ class TicTacToe: UIViewController {
     private var activePlayer = 1 {
         didSet {
             if activePlayer == 1 {
-                player1Container.backgroundColor = UIColor.flatRed()
+                player1Container.backgroundColor = UIColor.flatOrange()
                 player2Container.backgroundColor = UIColor.clear
             } else {
                 player1Container.backgroundColor = UIColor.clear
-                player2Container.backgroundColor = UIColor.flatBlue()
+                player2Container.backgroundColor = UIColor.flatSkyBlue()
+               
             }
         }
     }
@@ -71,28 +80,63 @@ class TicTacToe: UIViewController {
         ]
     
 
-    
-    
+    var tempMax = Int()
+    var tempMin = Int()
+    var receivedArray = [0,0,0]
+    var tempArray = [Int]()
+    var tempPlayerMode : String!
+    var playerMode = ""
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
-      gridHeightConstraint.constant = view.frame.width - 10
+        gridHeightConstraint.constant = view.frame.width - 10
         
-        setDistanceLabels()
+       
         
         playerHasWonLabel.isHidden = true
         playAgainOutlet.isHidden = true
         playerHasWonLabel.alpha = 1
         playAgainOutlet.alpha = 1
         player2Container.backgroundColor = UIColor.clear
-        player1Container.backgroundColor = UIColor.flatRed()
+        player1Container.backgroundColor = UIColor.flatOrange()
+        player2View.layer.cornerRadius = player2View.layer.frame.height / 2
+        player1View.layer.cornerRadius = player1View.layer.frame.height / 2
+
+        peerID = MCPeerID(displayName: UIDevice.current.name)
+        mcSession = MCSession(peer: peerID, securityIdentity: nil, encryptionPreference: .required)
+        mcSession.delegate = self
         
+      // sent via segues from previous VC
+        
+      lowerBound = tempMin
+      uppeerBound = tempMax
+      receivedArray = tempArray
+      playerMode = tempPlayerMode
+        
+        print("playerMode: \(playerMode)")
+        print("receivedArray: \(receivedArray)")
+        print("upperbound: \(uppeerBound), lowerbound: \(lowerBound)")
+        
+      setDistanceLabels()   
     }
 
-
+    
+    
+    
+    
+    private var lastPlayerToGo =  [MCPeerID]()
+    private var amountOfMovesMade = 0
+    
     
     @IBAction func BTNPressed(_ senderX: UIButton) {
+        
+       amountOfMovesMade += 1
+        
+        
+        if lastPlayerToGo.last != peerID || mcSession.connectedPeers.count == 0  {
+        
+        
         let puttDistance = senderX.titleLabel?.text
         var borderColor = UIColor()
         var currentPlayer = ""
@@ -110,7 +154,14 @@ class TicTacToe: UIViewController {
         
         sendAlert(title: currentPlayer , message: nil, sender: senderX)
        
+            
        
+            
+            print("lastplayerTogo: \(lastPlayerToGo)")
+        print("MyPeerId: \(peerID)")
+        print("peerIDCountArray: \(lastPlayerToGo.count)")
+           // print("conectedPeers: \(mcSession.connectedPeers)")
+      }
      
         
                 
@@ -162,6 +213,9 @@ class TicTacToe: UIViewController {
     
     func placeMarkForPlayer(sender: UIButton) {
         
+        let numberString = String(sender.tag)
+        send(colorName: numberString, sender: sender)
+        
         
         if (gameState[sender.tag - 1] == 0) && gameIsActive == true {
             gameState[sender.tag - 1] = activePlayer
@@ -180,6 +234,33 @@ class TicTacToe: UIViewController {
         }
         
         
+        checkForWinningCombination()
+    
+        gameIsActive = false
+    
+        for i in gameState {
+            if i == 0 && gameHasBeenWon == false
+            {
+                gameIsActive = true
+                break
+            }
+        }
+        
+        if gameIsActive == false && gameHasBeenWon == false
+        {
+            playerHasWonLabel.text = "IT WAS A DRAW"
+            playerHasWonLabel.isHidden = false
+            playAgainOutlet.isHidden = false
+            player1View.isHidden = true
+            player2View.isHidden = true
+        }
+    }
+    
+    
+    
+    
+    
+    func checkForWinningCombination(){
         
         for combination in winningCombinations {
             
@@ -226,31 +307,16 @@ class TicTacToe: UIViewController {
         }
         
         
-        gameIsActive = false
-        
-        
-        
-        for i in gameState {
-            if i == 0 && gameHasBeenWon == false
-            {
-                gameIsActive = true
-                break
-            }
-        }
-        
-        if gameIsActive == false && gameHasBeenWon == false
-        {
-            playerHasWonLabel.text = "IT WAS A DRAW"
-            playerHasWonLabel.isHidden = false
-            playAgainOutlet.isHidden = false
-            player1View.isHidden = true
-            player2View.isHidden = true
-        }
-        
-        
-        
-        
     }
+    
+    
+    
+    
+    
+    
+    
+    
+    
     
     @IBAction func playAgain(_ sender: UIButton) {
         gameState = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
@@ -284,12 +350,236 @@ class TicTacToe: UIViewController {
     // TODO: lowerbound and upperbound values need to be passed in from pickerView or other source.
     func setDistanceLabels(){
         
-        for num in BTN1.indices {
-            BTN1[num].setTitle("\(random(lowerBound..<uppeerBound))'", for: UIControlState())
-            BTN1[num].setTitleColor(UIColor.darkGray, for: UIControlState())
-            BTN1[num].titleLabel?.font = UIFont(name: "Avenir-Medium", size: 12)
+        if playerMode == "singlePlayer" {
+            for num in BTN1.indices {
+                BTN1[num].setTitle("\(random(lowerBound..<uppeerBound))'", for: UIControlState())
+                BTN1[num].setTitleColor(UIColor.darkGray, for: UIControlState())
+                BTN1[num].titleLabel?.font = UIFont(name: "Avenir-Medium", size: 12)
+                }
+        } else {
+            for num in BTN1.indices{
+                let NumberAtIndexSpot = receivedArray[num]
+                BTN1[num].setTitle("\(NumberAtIndexSpot)'", for: UIControlState())
+                BTN1[num].setTitleColor(UIColor.darkGray, for: UIControlState())
+                BTN1[num].titleLabel?.font = UIFont(name: "Avenir-Medium", size: 12)
+                
+            }
         }
+        
     }
 
+    
+
+    
+    func send(colorName : String, sender: UIButton) {
+     //   NSLog("%@", "sendColor: \(colorName) to \(mcSession.connectedPeers.count) peers")
+
+        if mcSession.connectedPeers.count > 0 {
+            do {
+                try self.mcSession.send(colorName.data(using: .utf8)!, toPeers: mcSession.connectedPeers, with: .reliable)
+                lastPlayerToGo.append(peerID)
+               
+             
+                
+            }
+            catch let error {
+                NSLog("%@", "Error for sending: \(error)")
+            }
+              print("tried sending data")
+        }
+    }
+    
+    
+    
+
+    
+    
+    // MARK: - Multipeer Connectivity
+    
+    var myAdvertiser = false   //bool to control if .stop() is allowed.
+    
+    @IBAction func showConnectivityActions(_ sender: Any) {
+        
+        let actionSheet = UIAlertController(title: "Use Two Devices For The Game", message: "This action will allow you to have control the game with two phones instead of one.", preferredStyle: .actionSheet)
+        
+        actionSheet.addAction(UIAlertAction(title: "Host Game", style: .default, handler: { (action:UIAlertAction) in
+            
+            self.mcAdvertiserAssistant = MCAdvertiserAssistant(serviceType: "ticTacToe", discoveryInfo: nil, session: self.mcSession)
+            self.mcAdvertiserAssistant.start()
+            self.myAdvertiser = true
+        }))
+        
+        if mcSession.connectedPeers.count < 2 {
+        actionSheet.addAction(UIAlertAction(title: "Join Game", style: .default, handler: { (action:UIAlertAction) in
+            let mcBrowser = MCBrowserViewController(serviceType: "ticTacToe", session: self.mcSession)
+            mcBrowser.delegate = self
+            self.present(mcBrowser, animated: true, completion: nil)
+        }))
+        }
+        
+        if mcSession.connectedPeers.count > 0 || myAdvertiser == true {
+                    var _title = "Leave Current Game"
+            if myAdvertiser == true && mcSession.connectedPeers.count == 0 {
+                _title = "Turn Off Host Signal"
+            }
+            
+            
+            
+                actionSheet.addAction(UIAlertAction(title: _title , style: .default, handler: { (action:UIAlertAction) in
+                    //TODO: safeproof this, maybe if mcSession is > 0 ? maybe only use one of them?
+                    self.mcSession.disconnect()
+                    
+                    if self.myAdvertiser == true {
+                        self.mcAdvertiserAssistant.stop()
+                        print("advertiser stopped ")
+                        self.myAdvertiser = false
+                        }
+                    
+                    }))
+        }
+        
+        
+        
+        
+        actionSheet.addAction(UIAlertAction(title: "Cancel", style: .cancel, handler: nil))
+        
+        self.present(actionSheet, animated: true, completion: nil)
+        
+    }
+    
+    
+    
+    
+    func session(_ session: MCSession, peer peerID: MCPeerID, didChange state: MCSessionState) {
+        switch state {
+        case MCSessionState.connected:
+            print("Connected: \(peerID.displayName)")
+            
+        case MCSessionState.connecting:
+            print("Connecting: \(peerID.displayName)")
+            
+        case MCSessionState.notConnected:
+            print("Not Connected: \(peerID.displayName)")
+        }
+    }
+    
+    func session(_ session: MCSession, didReceive data: Data, fromPeer peerID: MCPeerID) {
+        
+        
+        do {
+            let buttonPressed = String(data: data, encoding: .utf8)!
+            let buttonWithCorrectIndex = (Int(buttonPressed)! - 1)
+            
+            DispatchQueue.main.async {
+                print("buttonPressed Via sent transmission: \(String(describing: buttonPressed))")
+                
+              
+                if (self.gameState[buttonWithCorrectIndex] == 0) && self.gameIsActive == true {
+                    self.gameState[buttonWithCorrectIndex] = self.activePlayer
+                    
+                if (self.activePlayer == 1) {
+                    self.BTN1[buttonWithCorrectIndex].setBackgroundImage(UIImage(named:"cross_black"), for: UIControlState())
+                    self.BTN1[buttonWithCorrectIndex].setTitle("", for: UIControlState())
+                    
+                } else {
+                    self.BTN1[buttonWithCorrectIndex].setBackgroundImage(UIImage(named:"circle_black"), for: UIControlState())
+                    self.BTN1[buttonWithCorrectIndex].setTitle("", for: UIControlState())
+                   
+                    }
+                }
+                
+                
+                if self.activePlayer == 1 {
+                    self.activePlayer = 2
+                }else {
+                    self.activePlayer = 1
+                }
+                
+                
+                
+                self.checkForWinningCombination()
+                self.lastPlayerToGo.append(peerID)
+                
+              
+                self.amountOfMovesMade += 1
+
+               
+            }
+
+           
+        }catch{
+            fatalError("Unable to process recieved data")
+        }
+
+    }
+    
+    
+    
+    func session(_ session: MCSession, didReceive stream: InputStream, withName streamName: String, fromPeer peerID: MCPeerID) {
+        
+    }
+    
+   
+    
+    
+    
+    
+    
+    
+    
+    
+    func session(_ session: MCSession, didStartReceivingResourceWithName resourceName: String, fromPeer peerID: MCPeerID, with progress: Progress) {
+        
+    }
+    
+    func session(_ session: MCSession, didFinishReceivingResourceWithName resourceName: String, fromPeer peerID: MCPeerID, at localURL: URL?, withError error: Error?) {
+        
+    }
+    
+    
+    func browserViewControllerDidFinish(_ browserViewController: MCBrowserViewController) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+    func browserViewControllerWasCancelled(_ browserViewController: MCBrowserViewController) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+    
+    
+    
+    
+    
+    
+
+    
+    
+    
+    
+    
+    
+    
+    
+ 
+    
 
 }
+
+//extension String {
+//    //: ### Base64 encoding a string
+//    func base64Encoded() -> String? {
+//        if let data = self.data(using: .utf8) {
+//            return data.base64EncodedString()
+//        }
+//        return nil
+//    }
+//
+//    //: ### Base64 decoding a string
+//    func base64Decoded() -> String? {
+//        if let data = Data(base64Encoded: self) {
+//            return String(data: data, encoding: .utf8)
+//        }
+//        return nil
+//    }
+//}
+
